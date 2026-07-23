3d1dc26b51517b144fdc7040b3d76494f3f6190b
emortal <emortaldev@proton.me>
2026-07-23 20:21:50 +0100

    use neoverse march

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ subprojects {
                     nativeImageCapable = true
                 }
 
-                buildArgs.add("-march=native")
+                buildArgs.add("-march=neoverse-n1")
                 buildArgs.add("--enable-url-protocols=http,https")
                 buildArgs.add("--initialize-at-build-time=com.alibaba.fastjson2")
                 buildArgs.add("--enable-native-access=ALL-UNNAMED")
